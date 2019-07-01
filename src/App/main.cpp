f8153983c42538008b95c33a30625313dbab39d8
Alexander Tyulenev <sealson.tgn@gmail.com>
2019-07-01 08:07:44 +0300

    clean main

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -1,15 +1,9 @@
 #include <QApplication>
-#include <QThread>
-#include "mainwindow.h"
 #include "mapwidget.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow mw;
-//    mw.setWindowTitle("GNSS widget environment");
-//    mw.resize(640, 480);
-//    mw.show();
 
     MapWidget mw;
     mw.setWindowTitle("GNSS widget environment");
